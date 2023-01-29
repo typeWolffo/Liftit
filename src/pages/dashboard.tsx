e937be142620312864e10a4b05f88832565de2c5
Jakub Wilk <jwilk436@gmail.com>
2023-01-29 21:52:59 +0100

    Add full supabase integration

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,10 @@
 import { api } from "../utils/api";
 import { Button } from "../ui/Button";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 function Dashboard() {
+  const { data: sessionData } = useSession();
   const { mutate } = api.exercise.create.useMutation();
 
   const handleMutate = () => {
@@ -12,9 +15,25 @@ function Dashboard() {
   };
 
   return (
-    <div>
-      <span>dashboard</span>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-background to-dark-700">
+      <div className="flex flex-col items-center justify-center font-primary text-primary">
+        <p>Logged as {sessionData?.user?.name}</p>
+        <Image
+          className="my-5 rounded-full"
+          src={sessionData?.user?.image as string}
+          alt=""
+          width={96}
+          height={96}
+        />
+      </div>
       <Button onClick={handleMutate}>Rickroll</Button>
+      <Button
+        variant="primary-outlined"
+        style={{ marginTop: "5px" }}
+        onClick={() => void signOut()}
+      >
+        Logout
+      </Button>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,9 @@ const Home: NextPage = () => {
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-background to-dark-700">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="relative h-[100px] w-[90%]">
-            <Image src="/images/h100.svg" alt={"Payin logo"} fill priority />
-          </div>
+          <h1 className="font-primary text-9xl font-bold text-primary">
+            LiftIT
+          </h1>
           <Button
             variant="primary-outlined"
             onClick={sessionData ? () => void signOut() : () => void signIn()}
@@ -21,21 +21,6 @@ const Home: NextPage = () => {
             {sessionData ? "Sign out" : "Let's go"}
           </Button>
         </div>
-        <p className="font-secondary text-primary">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur
-        </p>
       </main>
     </>
   );
